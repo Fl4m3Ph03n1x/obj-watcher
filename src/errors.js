ff09475a2f10926cb36d24ac54bfa03309d60c93
Fl4m3Ph03n1x <palmelapedro@gmail.com>
2017-08-25 13:34:37 +0200

    Fixing lint issues.

--- src/errors.js
+++ src/errors.js
@@ -3,15 +3,15 @@
  *  @augments   {Error}
  *  @property   {string}    name=ObjectNotWatched   Name of the error.
  *  @property   {string}    message                 Message of the error.
- * 
- *  @description    Error thrown when one tries to perform an action on an 
- *                  object that was not previously registered using 
+ *
+ *  @description    Error thrown when one tries to perform an action on an
+ *                  object that was not previously registered using
  *                  <code>watch</code>.
- * 
- * @example 
- * 
+ *
+ * @example
+ *
  * const watcher = require("obj-watcher");
- * 
+ *
  * watcher.unwatch("status"); //Throw, "status" is not watched.
  */
 
@@ -19,17 +19,17 @@
  *  @typedef    {Error}     ObjectAlreadyWatched
  *  @property   {string}    name=ObjectAlreadyWatched   Name of the error.
  *  @property   {string}    message                     Message of the error.
- * 
- *  @description    Error thrown when one tries to re-watch an object that was 
- *                  already being watched. This means that you are trying to 
+ *
+ *  @description    Error thrown when one tries to re-watch an object that was
+ *                  already being watched. This means that you are trying to
  *                  watch an object and that the library already has something
  *                  with the given {Id}.
- * 
- * @example 
- * 
+ *
+ * @example
+ *
  * const watcher = require("obj-watcher");
- * 
- * watcher.watch("status", { online: true });   
+ *
+ * watcher.watch("status", { online: true });
  * watcher.watch("status", { fruit: "banana" }); //Throws, key "status" is already under use.
  */
 
@@ -37,17 +37,17 @@
  *  @typedef    {Error}     CallbackNotAFunction
  *  @property   {string}    name=CallbackNotAFunction   Name of the error.
  *  @property   {string}    message                     Message of the error.
- * 
- *  @description    Error throw when one calls <code>onChange</code> passing a  
+ *
+ *  @description    Error throw when one calls <code>onChange</code> passing a
  *                  callback parameter that is not a function.
- * 
- * @example 
- * 
+ *
+ * @example
+ *
  * const watcher = require("obj-watcher");
- * 
- * watcher.watch("status", { online: true });   
+ *
+ * watcher.watch("status", { online: true });
  * //Throws, second parameter should be of type "function".
- * watcher.onChange("status", "I am not a function!");    
+ * watcher.onChange("status", "I am not a function!");
  */
 
 /**
@@ -56,13 +56,13 @@
  * @param   {string}        name    The name of the error to be created.
  * @param   {string}        message The message the error will contain.
  * @returns {Error}
- * 
+ *
  * @description Creates an error and returns it. Only creates generic errors of
  *              type "Error".
  */
 const errorFactory = (name, message) => {
     const error = new Error();
-    error.message = message + " See https://fl4m3ph03n1x.github.io/obj-watcher/global.html for more info on errors.";
+    error.message = `${message} See https://fl4m3ph03n1x.github.io/obj-watcher/global.html for more info on errors.`;
     error.name = name;
     return error;
 };
@@ -70,10 +70,10 @@ const errorFactory = (name, message) => {
 /**
  *  @private
  *  @func       objectNotWatched
- *  @param      {Id}                objId   Id of the object that caused the 
+ *  @param      {Id}                objId   Id of the object that caused the
  *                                          error.
  *  @returns    {Error}
- * 
+ *
  *  @description    Creates a "ObjectNotWatched" error for the object with the
  *                  given id and returns it. Used when a user tries an operation
  *                  on an unknown object.
@@ -84,11 +84,11 @@ const objectNotWatched = objId =>
 /**
  *  @private
  *  @func       objectAlreadyWatched
- *  @param      {Id}            objId       Id of the object that caused the 
+ *  @param      {Id}            objId       Id of the object that caused the
  *                                          error.
  *  @returns    {Error}
- * 
- *  @description    Creates a "ObjectAlreadyWatched" error for the object with 
+ *
+ *  @description    Creates a "ObjectAlreadyWatched" error for the object with
  *                  the given id and returns it. Used when a user asks to watch
  *                  an object already being watched.
  */
@@ -98,12 +98,12 @@ const objectAlreadyWatched = objId =>
 /**
  *  @private
  *  @func       callbackNotAFunction
- *  @param      {Id}            objId   Id of the object that caused the 
+ *  @param      {Id}            objId   Id of the object that caused the
  *                                          error.
  *  @returns    {Error}
- * 
- *  @description    Creates a "CallbackNotAFunction" error for the object with 
- *                  the given id and returns it. Used when a user passes an 
+ *
+ *  @description    Creates a "CallbackNotAFunction" error for the object with
+ *                  the given id and returns it. Used when a user passes an
  *                  object that is expected to be a function but isn't.
  */
 const callbackNotAFunction = objId =>
